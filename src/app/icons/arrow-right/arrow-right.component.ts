7ebe552d4983f5b60ac7f718a689e00d2d0e405b
Josué Teixeira <zjosuez@outlook.com>
2024-03-08 22:27:19 -0300

    feat: add arrow-right image svg component

---
+++ src/app/icons/arrow-right/arrow-right.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'arrow-right',
+  standalone: true,
+  imports: [],
+  templateUrl: './arrow-right.component.html',
+})
+export class ArrowRightComponent {
+
+}
