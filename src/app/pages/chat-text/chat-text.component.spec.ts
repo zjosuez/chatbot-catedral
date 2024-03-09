df00e9b1dc73384bcd6ef12332082a8c4325a873
Josué Teixeira <zjosuez@outlook.com>
2024-03-09 12:15:07 -0300

    feat: Add chat-text component files

---
+++ src/app/pages/chat-text/chat-text.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChatTextComponent } from './chat-text.component';
+
+describe('ChatTextComponent', () => {
+  let component: ChatTextComponent;
+  let fixture: ComponentFixture<ChatTextComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ChatTextComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ChatTextComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/chat-text/chat-text.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-text',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-text.component.html',
+  styleUrl: './chat-text.component.scss'
+})
+export class ChatTextComponent {
+
+}
