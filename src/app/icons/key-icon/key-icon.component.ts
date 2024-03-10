cf0ef5cffa452db88cd414ac89803c2d458e6a0c
Josué Teixeira <zjosuez@outlook.com>
2024-03-09 22:04:36 -0300

    feat: Add key icon component

---
+++ src/app/icons/key-icon/key-icon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'key-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './key-icon.component.html',
+})
+export class KeyIconComponent {
+
+}
