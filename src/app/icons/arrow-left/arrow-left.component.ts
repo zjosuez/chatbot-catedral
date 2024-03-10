d48a2ff657cd43bf5913978af2de3f2c9552ff84
Josué Teixeira <zjosuez@outlook.com>
2024-03-09 22:03:46 -0300

    feat: Add arrow-left component

---
+++ src/app/icons/arrow-left/arrow-left.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'arrow-left',
+  standalone: true,
+  imports: [],
+  templateUrl: './arrow-left.component.html',
+})
+export class ArrowLeftComponent {
+
+}
