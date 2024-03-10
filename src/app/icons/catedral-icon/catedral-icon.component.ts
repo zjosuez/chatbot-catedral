a78b725034bfa663166b75e32915746eabe9d950
Josué Teixeira <zjosuez@outlook.com>
2024-03-09 22:04:10 -0300

    feat: Add catedral icon component

---
+++ src/app/icons/catedral-icon/catedral-icon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'catedral-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './catedral-icon.component.html',
+})
+export class CatedralIconComponent {
+
+}
