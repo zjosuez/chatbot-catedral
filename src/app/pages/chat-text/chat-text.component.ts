71e6d308d56b0d29290c2dc738684c1ea91e8658
Josué Teixeira <zjosuez@outlook.com>
2024-03-10 11:54:25 -0300

    feat: Add chat suggestions component to chat-text component

--- src/app/pages/chat-text/chat-text.component.ts
+++ src/app/pages/chat-text/chat-text.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CatedralIconComponent } from "../../icons/catedral-icon/catedral-icon.component";
 import { ArrowLeftComponent } from "../../icons/arrow-left/arrow-left.component";
+import { ChatSuggestionsComponent } from "../../components/chat-suggestions/chat-suggestions.component";
 
 @Component({
     selector: 'app-chat-text',
     standalone: true,
     templateUrl: './chat-text.component.html',
     styleUrl: './chat-text.component.scss',
-    imports: [CatedralIconComponent, ArrowLeftComponent]
+    imports: [CatedralIconComponent, ArrowLeftComponent, ChatSuggestionsComponent]
 })
 export class ChatTextComponent {
 
