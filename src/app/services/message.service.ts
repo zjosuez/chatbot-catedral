f027106193a702763312e8ae7c7541942e060bd9
Josué Teixeira <zjosuez@outlook.com>
2024-03-10 17:35:28 -0300

    feat: implement MessageService Integration

---
+++ src/app/services/message.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MessageResponse } from '../types/message-response.type';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+  private endPointUrl = "https://jdzrrnv67dcnspexppkqyugwli0iaykg.lambda-url.us-east-1.on.aws/";
+  constructor(private http: HttpClient) { }
+
+  sendData(message: string): Observable<MessageResponse> {
+    const data = {message};
+
+    return this.http.post<MessageResponse>(this.endPointUrl, data);
+  }
+}
