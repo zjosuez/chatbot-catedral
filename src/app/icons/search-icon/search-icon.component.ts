b6131eb67ff8f562ab33b8b93981f9e7b049404b
Josué Teixeira <zjosuez@outlook.com>
2024-03-09 22:04:44 -0300

    feat: Add search icon component

---
+++ src/app/icons/search-icon/search-icon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'search-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-icon.component.html',
+})
+export class SearchIconComponent {
+
+}
