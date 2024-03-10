519c891579403de1925b8a4f59913785c2425277
Josué Teixeira <zjosuez@outlook.com>
2024-03-09 22:04:21 -0300

    feat: Add interrogation icon component

---
+++ src/app/icons/interrogation-icon/interrogation-icon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'interrogation-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './interrogation-icon.component.html',
+})
+export class InterrogationIconComponent {
+
+}
