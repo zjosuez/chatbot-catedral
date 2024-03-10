250b2f6b8c29f3753f27a020df6b51c2e72bb4bb
Josué Teixeira <zjosuez@outlook.com>
2024-03-09 22:05:13 -0300

    feat: add chat component with the header html and css

--- src/app/pages/chat-text/chat-text.component.ts
+++ src/app/pages/chat-text/chat-text.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CatedralIconComponent } from "../../icons/catedral-icon/catedral-icon.component";
+import { ArrowLeftComponent } from "../../icons/arrow-left/arrow-left.component";
 
 @Component({
-  selector: 'app-chat-text',
-  standalone: true,
-  imports: [],
-  templateUrl: './chat-text.component.html',
-  styleUrl: './chat-text.component.scss'
+    selector: 'app-chat-text',
+    standalone: true,
+    templateUrl: './chat-text.component.html',
+    styleUrl: './chat-text.component.scss',
+    imports: [CatedralIconComponent, ArrowLeftComponent]
 })
 export class ChatTextComponent {
 
