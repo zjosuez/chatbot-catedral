c038c39604653400c57fbd2318500a910436141f
Josué Teixeira <zjosuez@outlook.com>
2024-03-11 22:55:44 -0300

    feat: Add vector-right svg component

---
+++ src/app/icons/vector-right/vector-right.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vector-right',
+  standalone: true,
+  imports: [],
+  templateUrl: './vector-right.component.html',
+})
+export class VectorRightComponent {
+
+}
