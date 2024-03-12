26b11dacd73fb4da81501a88019cc5ea88a7344e
Josué Teixeira <zjosuez@outlook.com>
2024-03-11 22:55:14 -0300

    refactor: Update chat-dialog component HTML, SCSS, and TypeScript files

--- src/app/components/chat-dialog/chat-dialog.component.ts
+++ src/app/components/chat-dialog/chat-dialog.component.ts
@@ -2,13 +2,14 @@ import { Component, Input } from '@angular/core';
 import { Message } from '../../types/message.type';
 import { ChatResponseComponent } from "../chat-response/chat-response.component";
 import { CommonModule } from '@angular/common';
+import { InputTextComponent } from "../input-text/input-text.component";
 
 @Component({
     selector: 'chat-dialog',
     standalone: true,
     templateUrl: './chat-dialog.component.html',
     styleUrl: './chat-dialog.component.scss',
-    imports: [ChatResponseComponent, CommonModule]
+    imports: [ChatResponseComponent, CommonModule, InputTextComponent]
 })
 export class ChatDialogComponent {
   @Input() messages: Message[] = [];
