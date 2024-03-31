65591a1b336cd29df2d085766891ec5a8e4a33a3
Josué Teixeira <zjosuez@outlook.com>
2024-03-31 15:23:36 -0300

    refactor: Adjusted the backend request endpoint

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -2,13 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MessageResponse } from '../types/message-response.type';
 import { Observable } from 'rxjs';
+import { env } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MessageService {
-  private endPointUrl =
-    'https://jdzrrnv67dcnspexppkqyugwli0iaykg.lambda-url.us-east-1.on.aws/';
+  private endPointUrl = env.apiUrl;
   constructor(private http: HttpClient) {}
 
   sendData(message: string): Observable<MessageResponse> {
