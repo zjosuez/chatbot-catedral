c6488be65d7a1f4a787ea618b9b8b6152909939e
Josué Teixeira <zjosuez@outlook.com>
2024-03-31 15:22:27 -0300

    feat: Implemented text input field for user queries

--- src/app/pages/chat-text/chat-text.component.ts
+++ src/app/pages/chat-text/chat-text.component.ts
@@ -8,30 +8,47 @@ import { ChatDialogComponent } from '../../components/chat-dialog/chat-dialog.co
 import { MessageService } from '../../services/message.service';
 import { MessageResponse } from '../../types/message-response.type';
 import { StorageService } from '../../services/storage.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { VectorRightComponent } from "../../icons/vector-right/vector-right.component";
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-chat-text',
-  standalone: true,
-  templateUrl: './chat-text.component.html',
-  styleUrl: './chat-text.component.scss',
-  imports: [
-    CatedralIconComponent,
-    ArrowLeftComponent,
-    ChatSuggestionsComponent,
-    CommonModule,
-    ChatDialogComponent,
-  ],
-  providers: [MessageService, StorageService],
+    selector: 'app-chat-text',
+    standalone: true,
+    templateUrl: './chat-text.component.html',
+    styleUrl: './chat-text.component.scss',
+    providers: [MessageService, StorageService],
+    imports: [
+        CatedralIconComponent,
+        ArrowLeftComponent,
+        ChatSuggestionsComponent,
+        CommonModule,
+        ChatDialogComponent,
+        VectorRightComponent,
+        ReactiveFormsModule
+    ]
 })
 export class ChatTextComponent {
+  chatForm!: FormGroup;
+
   constructor(
     private service: MessageService,
     private storageService: StorageService,
-  ) {}
+    private router: Router,
+  ) {
+    this.chatForm = new FormGroup({
+      message: new FormControl('', [Validators.required])
+    });
+  }
 
   questions: Message[] = JSON.parse(
     this.storageService.getItem('questions') ?? '[]',
   );
+
+  submit(){
+    this.sendQuestion(this.chatForm.value.message);
+    this.chatForm.reset();
+  }
 
   updateLocalStorage() {
     this.storageService.setItem('questions', JSON.stringify(this.questions));
@@ -56,5 +73,9 @@ export class ChatTextComponent {
         this.updateLocalStorage();
       },
     });
+  }
+
+  return() {
+    this.router.navigate(['']);
   }
 }
